39e53186415ab364fecc847bd0ff6d1ac0f8741b
Adesina Joshua <tolujosh1@gmail.com>
2024-12-09 10:36:48 +0100

    navbar incoming

--- gracious/src/components/Navbar/Navbar.jsx
+++ gracious/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <nav>
         <img src="" alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testimonals</li>
+            <li>Contact Us</li>
+        </ul>
     </nav>
   )
 }
