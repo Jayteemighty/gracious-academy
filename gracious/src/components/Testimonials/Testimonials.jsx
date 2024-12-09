7c640e2d4f3f069258839ffba35607db866ee6cd
Adesina Joshua <tolujosh1@gmail.com>
2024-12-09 17:24:27 +0100

    adjust testimonial completed

--- gracious/src/components/Testimonials/Testimonials.jsx
+++ gracious/src/components/Testimonials/Testimonials.jsx
@@ -43,10 +43,10 @@ const Testimonials = () => {
                             <img src={user_1} alt="" />
                             <div>
                                 <h3>Emily Williams</h3>
-                                <span>Edusity, USA</span>
+                                <span>Gracious Academy, USA</span>
                             </div>
                         </div>
-                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        <p>Choosing to pursue my degree at Gracious academy was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
                     </div>
                 </li>
                 <li>
@@ -55,10 +55,10 @@ const Testimonials = () => {
                             <img src={user_2} alt="" />
                             <div>
                                 <h3>William Jackson</h3>
-                                <span>Edusity, USA</span>
+                                <span>Gracious Academy, USA</span>
                             </div>
                         </div>
-                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        <p>Choosing to pursue my degree at Gracious academy was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
                     </div>
                 </li>
                 <li>
@@ -67,10 +67,10 @@ const Testimonials = () => {
                             <img src={user_3} alt="" />
                             <div>
                                 <h3>Emily Williams</h3>
-                                <span>Edusity, USA</span>
+                                <span>Gracious Academy, USA</span>
                             </div>
                         </div>
-                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        <p>Choosing to pursue my degree at Gracious academy was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
                     </div>
                 </li>
                 <li>
@@ -79,10 +79,10 @@ const Testimonials = () => {
                             <img src={user_4} alt="" />
                             <div>
                                 <h3>William Jackson</h3>
-                                <span>Edusity, USA</span>
+                                <span>Gracious Academy, USA</span>
                             </div>
                         </div>
-                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        <p>Choosing to pursue my degree at Gracious academy was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
                     </div>
                 </li>
             </ul>
