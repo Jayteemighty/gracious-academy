a594661e8542233d9ae47626d2958d48cac0cbe3
Adesina Joshua <tolujosh1@gmail.com>
2024-12-23 12:21:23 +0100

    adjust completed

--- gracious/src/components/Campus/Campus.jsx
+++ gracious/src/components/Campus/Campus.jsx
@@ -1,24 +1,41 @@
-
-import React from 'react'
-import './Campus.css'
-import gallery_1 from '../../assets/gallery-1.png'
-import gallery_2 from '../../assets/gallery-2.png'
-import gallery_3 from '../../assets/gallery-3.png'
-import gallery_4 from '../../assets/gallery-4.png'
-import white_arrow from '../../assets/white-arrow.png'
+import React from 'react';
+import './Campus.css';
+import campus_image from '../../assets/campus-image.png'; // Update with the correct path
+import email_icon from '../../assets/email-icon.png'; // Update with the correct path
+import whatsapp_icon from '../../assets/whatsapp-icon.png'; // Update with the correct path
 
 const Campus = () => {
-  return (
-    <div className='campus'>
-        <div className="gallery">
-            <img src={gallery_1} alt="" />
-            <img src={gallery_2} alt="" />
-            <img src={gallery_3} alt="" />
-            <img src={gallery_4} alt="" />
+    return (
+        <div className="campus-section">
+            <div className="campus-left">
+                <h3>Metodologia Gracious Academy</h3>
+                <p>
+                    A Gracious Academy possui uma das metodologias mais eficientes da atualidade. 
+                    Ela consiste no método imersivo, ou seja: você mergulha de cabeça na cultura e no 
+                    dia a dia de um país de língua inglesa e aprende do mesmo modo como aprendeu o português.
+                </p>
+                <h4>“SISTEMA NATURAL DE ENSINO”</h4>
+                <ul>
+                    <li>Inglês desde a primeira aula</li>
+                    <li>Abordagem comunicativa</li>
+                    <li>Sala de aula invertida</li>
+                    <li>Exames Internacionais: Cambridge, Celta e Toefl</li>
+                </ul>
+                <p>Receba novidades da Gracious Academy em seu e-mail!</p>
+                <div className="contact-options">
+                    <button className="email-btn">
+                        <img src={email_icon} alt="Email Icon" /> Enviar e-mail
+                    </button>
+                    <button className="whatsapp-btn">
+                        <img src={whatsapp_icon} alt="WhatsApp Icon" /> Enviar WhatsApp
+                    </button>
+                </div>
+            </div>
+            <div className="campus-right">
+                <img src={campus_image} alt="Campus" />
+            </div>
         </div>
-        <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
-    </div>
-  )
-}
+    );
+};
 
-export default Campus
+export default Campus;
